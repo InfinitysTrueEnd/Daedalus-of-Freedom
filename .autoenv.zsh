e1efe5f0faba05d84c7c74b8d4878a26cee4f2ee
HiddenKn <133887+HiddenKn@users.noreply.github.com>
2018-11-01 13:04:06 +0100

    Made autoenv file automatically install and source byond.

--- .autoenv.zsh
+++ .autoenv.zsh
@@ -1,3 +1,6 @@
 #Warning: Always check your autoenv file before accepting it.
 export BYOND_MAJOR=`echo $(awk -F "=" '/BYOND_MAJOR/ {print $2}' .travis.yml|tr -d '"')`
 export BYOND_MINOR=`echo $(awk -F "=" '/BYOND_MINOR/ {print $2}' .travis.yml|tr -d '"')`
+
+sh install-byond.sh&>/dev/null
+source $HOME/BYOND-$BYOND_MAJOR.$BYOND_MINOR/byond/bin/byondsetup
